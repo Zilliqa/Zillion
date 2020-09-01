277e3560504f0bf79c146af12d3637f6c82aa939
teye <teye@zilliqa.com>
2020-09-01 16:59:33 +0800

    refactor modal components

--- src/components/contract-calls/update-commission-rate.tsx
+++ src/components/contract-calls/update-commission-rate.tsx
@@ -1,65 +1,85 @@
 import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { trackPromise } from 'react-promise-tracker';
 import { OperationStatus } from "../../util/enum";
 import * as Account from "../../account";
+import Alert from '../alert';
+
+import ModalPending from '../contract-calls-modal/modal-pending';
+import ModalSent from '../contract-calls-modal/modal-sent';
 
 function UpdateCommRateModal(props: any) {
     const { proxy, currentRate } = props;
     const [newRate, setNewRate] = useState('');
     const [txnId, setTxnId] = useState('')
     const [error, setError] = useState('');
+    const [isPending, setIsPending] = useState('');
 
     const handleClose = () => {
         // reset state
         // timeout to wait for modal to fade out before clearing
         // so that the animation is smoother
+        toast.dismiss();
         setTimeout(() => {
             setNewRate('');
             setTxnId('');
             setError('');
+            setIsPending('');
         }, 150);
     }
 
     const updateCommRate = async () => {
-        const result = await Account.updateCommissionRate(proxy, newRate);
-        console.log(result);
-        if (result === OperationStatus.ERROR) {
-            setError(OperationStatus.ERROR);
-        } else {
-            const resultJson = JSON.parse(result);
-            setTxnId(resultJson.txn_id);
-        }
+        setIsPending(OperationStatus.PENDING);
+        trackPromise(Account.updateCommissionRate(proxy, newRate)
+            .then((result) => {
+                console.log(result);
+                if (result === OperationStatus.ERROR) {
+                    setError(OperationStatus.ERROR);
+                    Alert('error', "There is an error. Please try again.");
+                } else {
+                    const resultJson = JSON.parse(result);
+                    setTxnId(resultJson.txn_id);
+                }
+            }).finally(() => {
+                setIsPending('');
+            }));
     }
 
     return (
         <div id="update-comm-rate-modal" className="modal fade" tabIndex={-1} role="dialog" aria-labelledby="updateCommRateModalLabel" aria-hidden="true">
             <div className="modal-dialog" role="document">
                 <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="updateCommRateModalLabel">Update Commission Rate</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={handleClose}>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        { error ? ( <p>There is an error.</p> ) : null}
+                    {
+                        isPending ?
                         
-                        { txnId ?
-                            (
-                                <>
-                                <p>Transaction Sent: {txnId}</p>
-                                <p>Please check the status on Devex</p>
-                                </>
-                            ) :
-                            <>
+                        <ModalPending />
+
+                        :
+
+                        txnId ?
+
+                        <ModalSent txnId={txnId} handleClose={handleClose} />
+
+                        :
+
+                        <>
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="updateCommRateModalLabel">Update Commission Rate</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={handleClose}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
                             <p>Current Commission Rate: {currentRate ? currentRate : '0'} &#37;</p>
                             <input type="text" className="form-control mb-4" value={newRate} onChange={(e:any) => setNewRate(e.target.value)} placeholder="Enter new rate in %" />
                             <button type="button" className="btn btn-user-action mr-2" onClick={updateCommRate}>Update</button>
                             <button type="button" className="btn btn-user-action-cancel mx-2" data-dismiss="modal" onClick={handleClose}>Cancel</button>
-                            </>
-                        }
-                    </div>
+                        </div>
+                        </>
+                    }
                 </div>
             </div>
+            <ToastContainer hideProgressBar={true}/>
         </div>
     );
 }
--- src/components/contract-calls/update-receiver-address.tsx
+++ src/components/contract-calls/update-receiver-address.tsx
@@ -1,65 +1,85 @@
 import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { trackPromise } from 'react-promise-tracker';
 import { OperationStatus } from "../../util/enum";
 import * as Account from "../../account";
+import Alert from '../alert';
+
+import ModalPending from '../contract-calls-modal/modal-pending';
+import ModalSent from '../contract-calls-modal/modal-sent';
 
 function UpdateReceiverAddress(props: any) {
     const { proxy, currentReceiver } = props;
     const [newAddress, setNewAddress] = useState('');
     const [txnId, setTxnId] = useState('');
     const [error, setError] = useState('');
+    const [isPending, setIsPending] = useState('');
 
     const handleClose = () => {
         // reset state
         // timeout to wait for modal to fade out before clearing
         // so that the animation is smoother
+        toast.dismiss();
         setTimeout(() => {
             setNewAddress('');
             setTxnId('');
             setError('');
+            setIsPending('');
         }, 150);
     }
 
     const updateAddress = async () => {
-        const result = await Account.updateReceiverAddress(proxy, newAddress);
-        console.log(result);
-        if (result === OperationStatus.ERROR) {
-            setError(OperationStatus.ERROR);
-        } else {
-            const resultJson = JSON.parse(result);
-            setTxnId(resultJson.txn_id);
-        }
+        setIsPending(OperationStatus.PENDING);
+        trackPromise(Account.updateReceiverAddress(proxy, newAddress)
+            .then((result) => {
+                console.log(result);
+                if (result === OperationStatus.ERROR) {
+                    setError(OperationStatus.ERROR);
+                    Alert('error', "There is an error. Please try again.");
+                } else {
+                    const resultJson = JSON.parse(result);
+                    setTxnId(resultJson.txn_id);
+                }
+            })
+            .finally(() => {
+                setIsPending('');
+            }));
     }
 
     return (
         <div id="update-recv-addr-modal" className="modal fade" tabIndex={-1} role="dialog" aria-labelledby="updateRecvAddrModalLabel" aria-hidden="true">
             <div className="modal-dialog" role="document">
                 <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="updateRecvAddrModalLabel">Update Received Address</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={handleClose}>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        { error ? ( <p>There is an error.</p> ) : null}
-                        
-                        { txnId ?
-                            (
-                                <>
-                                <p>Transaction Sent: {txnId}</p>
-                                <p>Please check the status on Devex</p>
-                                </>
-                            ) :
-                            <>
+                    {
+                        isPending ?
+
+                        <ModalPending />
+
+                        :
+
+                        txnId ?
+
+                        <ModalSent txnId={txnId} handleClose={handleClose} />
+
+                        :
+                        <>
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="updateRecvAddrModalLabel">Update Received Address</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={handleClose}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
                             <p>Current Received Address: {currentReceiver ? currentReceiver : 'none'}</p>
                             <input type="text" className="form-control mb-4" value={newAddress} onChange={(e:any) => setNewAddress(e.target.value)} placeholder="Enter new address in ByStr20" />
                             <button type="button" className="btn btn-user-action mr-2" onClick={updateAddress}>Update</button>
                             <button type="button" className="btn btn-user-action-cancel mx-2" data-dismiss="modal" onClick={handleClose}>Cancel</button>
-                            </>
-                        }
-                    </div>
+                        </div>
+                        </>
+                    }
                 </div>
             </div>
+            <ToastContainer hideProgressBar={true}/>
         </div>
     );
 }
