e6ff13c5e05146297d7e8188ad0456f1ded1c286
teye <teye@zilliqa.com>
2020-09-02 16:30:46 +0800

    add additional transport method

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -5,10 +5,14 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function Alert(type: string, message: string) {
     switch (type) {
+        case 'info':
+            return toast.info(<div>{message}</div>);
         case 'error':
-            return toast.error(
-            <div>{message}</div>
-            );
+            return toast.error(<div>{message}</div>);
+        case 'success':
+            return toast.success(<div>{message}</div>);
+        case 'warn':
+            return toast.warn(<div>{message}</div>);
         default:
             return toast(message);
     }
--- src/components/wallet-ledger.tsx
+++ src/components/wallet-ledger.tsx
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import { AuthContext } from '../contexts/authContext';
 import Alert from './alert';
 import TransportU2F from "@ledgerhq/hw-transport-u2f";
+import TransportWebAuthn from "@ledgerhq/hw-transport-webauthn";
 import ZilliqaLedger from '../ledger';
 
 function LedgerWallet(props: any) {
@@ -12,10 +13,19 @@ function LedgerWallet(props: any) {
     const unlockWallet = async () => {
         console.log("unlock by hardware ledger");
 
-        const transport = await TransportU2F.create();
-        const ledger = new ZilliqaLedger(transport);
+        let transport = null;
+        const isWebAuthn = await TransportWebAuthn.isSupported();
+        if (isWebAuthn) {
+            console.log("webauthn is supported");
+            transport = await TransportWebAuthn.create();
+        } else {
+            transport = await TransportU2F.create();
+        }
 
         try {
+            const ledger = new ZilliqaLedger(transport);
+            Alert('info', "Please confirm action on Ledger Device.")
+
             const result = await ledger.getPublicAddress(0);
             console.log("unlock ledger success - public address...: %o", result.pubAddr);
             authContext.toggleAuthentication(result.pubAddr);
@@ -26,9 +36,6 @@ function LedgerWallet(props: any) {
             console.error("error unlocking ledger...:%o", err);
             Alert('error', 'There is something wrong with accessing the ledger. Have you unlock the PIN code?');
         }
-        // if no error
-        // call parent function to redirect to dashboard
-        // props.onSuccessCallback();
     }
 
     return (
