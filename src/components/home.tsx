d7c2d79f936d68ea0a288b41b8f7415e27a48fc8
teye <teye@zilliqa.com>
2020-10-06 10:56:36 +0800

    add staking viewer link at home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -356,9 +356,13 @@ function Home(props: any) {
             }
           </div>
           <footer id="disclaimer" className="align-items-start">
-            <div className="p-2">
-              <span className="ml-4 mx-3">&copy; 2020 Zilliqa</span> 
+            <div className="p-2 d-block">
+              <span className="ml-4 mx-3 align-middle">&copy; 2020 Zilliqa</span> 
               <button type="button" className="btn shadow-none" data-toggle="modal" data-target="#disclaimer-modal" data-keyboard="false" data-backdrop="static">Disclaimer</button>
+              <a className="footer-link mx-3 align-middle" 
+                  href={networks_config[selectedNetwork].node_status ?
+                        networks_config[selectedNetwork].node_status : 
+                        "https://zilliqa.com/"} target="_blank" rel="noopener noreferrer">Nodes Status</a>
             </div>
           </footer>
           <DisclaimerModal />
