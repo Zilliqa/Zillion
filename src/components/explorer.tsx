05f109cb8da600f090f21e74512ad6999c8c3215
teye <teye@zilliqa.com>
2020-10-22 21:06:15 +0800

    fix pending withdrawal

--- src/components/explorer.tsx
+++ src/components/explorer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { trackPromise } from 'react-promise-tracker';
 import DisclaimerModal from './disclaimer';
 import Footer from './footer';
@@ -59,22 +59,8 @@ function Explorer(props: any) {
     // eslint-disable-next-line
     const darkMode = useDarkMode(true);
 
-    
-    useEffect(() => {
-        let globalAPY = initDelegStats.globalAPY;
-        let zilRewards = initDelegStats.zilRewards;
-        let gzilRewards = initDelegStats.gzilRewards;
-        let gzilBalance = initDelegStats.gzilBalance;
-        let totalPendingWithdrawal = initDelegStats.totalPendingWithdrawal;
-        let totalDeposits = initDelegStats.totalDeposits;
+    const getWalletAddress = useCallback(() => {
         let wallet = '';
-        let stakedNodesList: DelegStakingPortfolioStats[] = [];
-
-        // for pending withdrawal progress
-        let totalClaimableAmtBN = new BigNumber(0); // Qa
-        let pendingStakeWithdrawalList: { amount: string, blkNumCountdown: string, blkNumCheck: string, progress: string }[] = [];
-        let progress = '0';
-
 
         if (validation.isBech32(address)) {
             // bech32
@@ -92,6 +78,19 @@ function Explorer(props: any) {
             // invalid address
             wallet = '';
         }
+
+        return wallet;
+    }, [address]);
+    
+    useEffect(() => {
+        let globalAPY = initDelegStats.globalAPY;
+        let zilRewards = initDelegStats.zilRewards;
+        let gzilRewards = initDelegStats.gzilRewards;
+        let gzilBalance = initDelegStats.gzilBalance;
+        let totalPendingWithdrawal = initDelegStats.totalPendingWithdrawal;
+        let totalDeposits = initDelegStats.totalDeposits;
+        let wallet = getWalletAddress();
+        let stakedNodesList: DelegStakingPortfolioStats[] = [];
 
         if (mountedRef.current) {
             setWalletBase16Address(wallet);
@@ -148,59 +147,6 @@ function Explorer(props: any) {
                 // compute gzil rewards
                 // converted to gzil when display
                 gzilRewards = totalZilRewardsBN;
-
-                // compute pending withdrawal progress
-                const withdrawalPendingState = await ZilliqaAccount.getImplStateExplorer(impl, networkURL, 'withdrawal_pending');
-
-                if (withdrawalPendingState.hasOwnProperty('withdrawal_pending')) {
-                    if (withdrawalPendingState['withdrawal_pending'].hasOwnProperty(wallet)) {
-                        const blkNumPendingWithdrawal = withdrawalPendingState['withdrawal_pending'][wallet];
-
-                        // get min bnum req
-                        const blkNumReqState = await ZilliqaAccount.getImplStateExplorer(impl, networkURL, 'bnum_req');
-                        const blkNumReq = blkNumReqState['bnum_req'];
-
-                        const currentBlkNum = new BigNumber(await ZilliqaAccount.getNumTxBlocksExplorer(networkURL)).minus(1);
-
-                        // compute each of the pending withdrawal progress
-                        for (const blkNum in blkNumPendingWithdrawal) {
-                            if (!blkNumPendingWithdrawal.hasOwnProperty(blkNum)) {
-                                continue;
-                            }
-
-                            // compute each pending stake withdrawal progress
-                            let pendingAmt = new BigNumber(blkNumPendingWithdrawal[blkNum]);
-                            let blkNumCheck = new BigNumber(blkNum).plus(blkNumReq);
-                            let blkNumCountdown = blkNumCheck.minus(currentBlkNum); // may be negative
-                            let completed = new BigNumber(0);
-
-                            // compute progress using blk num countdown ratio
-                            if (blkNumCountdown.isLessThanOrEqualTo(0)) {
-                                // can withdraw
-                                totalClaimableAmtBN = totalClaimableAmtBN.plus(pendingAmt);
-                                blkNumCountdown = new BigNumber(0);
-                                completed = new BigNumber(1);
-                            } else {
-                                // still have pending blks
-                                // 1 - (countdown/blk_req)
-                                const processed = blkNumCountdown.dividedBy(blkNumReq);
-                                completed = new BigNumber(1).minus(processed);
-                            }
-
-                            // convert progress to percentage
-                            progress = completed.times(100).toFixed(2);
-
-                            // record the stake withdrawal progress
-                            pendingStakeWithdrawalList.push({
-                                amount: pendingAmt.toString(),
-                                blkNumCountdown: blkNumCountdown.toString(),
-                                blkNumCheck: blkNumCheck.toString(),
-                                progress: progress.toString(),
-                            });
-                        }
-                    }
-                }
-
             })
             .catch((err) => {
                 console.error(err);
@@ -218,14 +164,85 @@ function Explorer(props: any) {
                     }
                     setDelegStats(data);
                     setStakedNodeList([...stakedNodesList]);
-                    setPendingWithdrawlList([...pendingStakeWithdrawalList]);
-                    setTotalClaimableAmt(totalClaimableAmtBN.toString())
-                    
                 }
             }), PromiseArea.PROMISE_GET_EXPLORER_STATS);
 
-    }, [impl, networkURL, address]);
+    }, [impl, networkURL, address, getWalletAddress]);
 
+
+    // compute pending withdrawal progress
+    useEffect(() => {
+        let wallet = getWalletAddress();
+        let totalClaimableAmtBN = new BigNumber(0); // Qa
+        let pendingStakeWithdrawalList: { amount: string, blkNumCountdown: string, blkNumCheck: string, progress: string }[] = [];
+        let progress = '0';
+
+
+        trackPromise(ZilliqaAccount.getImplStateExplorer(impl, networkURL, 'withdrawal_pending')
+            .then(async (contractState) => {
+                if (contractState === undefined || contractState === 'error') {
+                    return null;
+                }
+    
+                if (!contractState['withdrawal_pending'].hasOwnProperty(wallet)) {
+                    return null;
+                }
+
+                const blkNumPendingWithdrawal = contractState['withdrawal_pending'][wallet];
+
+                // get min bnum req
+                const blkNumReqState = await ZilliqaAccount.getImplStateExplorer(impl, networkURL, 'bnum_req');
+                const blkNumReq = blkNumReqState['bnum_req'];
+                const currentBlkNum = new BigNumber(await ZilliqaAccount.getNumTxBlocksExplorer(networkURL)).minus(1);
+
+                // compute each of the pending withdrawal progress
+                for (const blkNum in blkNumPendingWithdrawal) {
+                    if (!blkNumPendingWithdrawal.hasOwnProperty(blkNum)) {
+                        continue;
+                    }
+        
+                    // compute each pending stake withdrawal progress
+                    let pendingAmt = new BigNumber(blkNumPendingWithdrawal[blkNum]);
+                    let blkNumCheck = new BigNumber(blkNum).plus(blkNumReq);
+                    let blkNumCountdown = blkNumCheck.minus(currentBlkNum); // may be negative
+                    let completed = new BigNumber(0);
+        
+                    // compute progress using blk num countdown ratio
+                    if (blkNumCountdown.isLessThanOrEqualTo(0)) {
+                        // can withdraw
+                        totalClaimableAmtBN = totalClaimableAmtBN.plus(pendingAmt);
+                        blkNumCountdown = new BigNumber(0);
+                        completed = new BigNumber(1);
+                    } else {
+                        // still have pending blks
+                        // 1 - (countdown/blk_req)
+                        const processed = blkNumCountdown.dividedBy(blkNumReq);
+                        completed = new BigNumber(1).minus(processed);
+                    }
+        
+                    // convert progress to percentage
+                    progress = completed.times(100).toFixed(2);
+        
+                    // record the stake withdrawal progress
+                    pendingStakeWithdrawalList.push({
+                        amount: pendingAmt.toString(),
+                        blkNumCountdown: blkNumCountdown.toString(),
+                        blkNumCheck: blkNumCheck.toString(),
+                        progress: progress.toString(),
+                    });
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+                return null;
+            })
+            .finally(() => {
+                if (mountedRef.current) {
+                    setPendingWithdrawlList([...pendingStakeWithdrawalList]);
+                    setTotalClaimableAmt(totalClaimableAmtBN.toString())
+                }
+            }), PromiseArea.PROMISE_GET_EXPLORER_PENDING_WITHDRAWAL);
+    }, [impl, networkURL, getWalletAddress]);
 
     const redirectToMain = () => {
         props.history.push("/");
