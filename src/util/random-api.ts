0937ceb0a5b4ed91379729a92f9eebec751dfa98
teye <teye@zilliqa.com>
2020-11-28 21:57:46 +0800

    change random method

--- src/util/random-api.ts
+++ src/util/random-api.ts
@@ -3,7 +3,11 @@
  * used to minimize load on main API
  */
 
+
 import { NetworkURL } from "./enum";
+
+const { Random, MersenneTwister19937 } = require("random-js");
+const randomJS = new Random(MersenneTwister19937.autoSeed());
 
 // Testnet
 const TESTNET_API_LIST = [
@@ -26,7 +30,7 @@ const MAINNET_API_LIST = [
     "https://zilliqa-api.staked.cloud",
 ];
 
-// "https://staking-zil.kucoin.com/api", CORS error
+// https://staking-zil.kucoin.com/api", CORS error
 // https://ssn-zilliqa.moonlet.network/api , CORS error
 
 
@@ -37,21 +41,13 @@ export const getRandomAPI = (networkURL: string) => {
     // network URL should be api.zilliqa.com or dev-api.zilliqa.com
     // if it is testnet or mainnet
     if (networkURL === NetworkURL.MAINNET) {
-        const index = getRandomIndex(MAINNET_API_LIST.length-1);
+        const index = randomJS.integer(0, (MAINNET_API_LIST.length-1));
         api = MAINNET_API_LIST[index];
     } else {
         // everything else is testnet
-        const index = getRandomIndex(TESTNET_API_LIST.length-1);
+        const index = randomJS.integer(0, (TESTNET_API_LIST.length-1));
         api = TESTNET_API_LIST[index];
     }
     console.log("utilizing random api: %o", api);
     return api;
 };
-
-// fetch random array index
-// max is (array_length - 1)
-// return a number between 0 inclusive to (array_length - 1) inclusive
-function getRandomIndex(max:number) {
-    const min = 0;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
