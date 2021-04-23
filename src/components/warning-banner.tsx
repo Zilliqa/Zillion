906223f814958a6c3d70fff8a07f02ca91d3ed81
teye <teye@zilliqa.com>
2021-04-23 11:08:24 +0800

    add testnet annoucement

--- src/components/warning-banner.tsx
+++ src/components/warning-banner.tsx
@@ -17,7 +17,10 @@ function WarningBanner(props: any) {
                     <div className="px-3 pb-3"><strong>Attention</strong>: Staking contract is going to be paused at 20 OCT 4:00 for upgrading. Please refer to the announcement at <a href="https://t.me/zilliqa/493" target="_blank" rel="noopener noreferrer">https://t.me/zilliqa/493</a>.</div>
                 }
                 </> :
+                <>
                 <div className="p-3"><strong>Warning</strong>: Zillion is still in testnet. You are using this dApp at your own risk. Zilliqa cannot assume any responsibility for any loss of funds.</div>
+                <div className="px-3 pb-3"><strong>Attention</strong>: Testnet staking contract is going to be paused for updates.</div>
+                </>
             }
         </div>
     );
