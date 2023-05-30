7eab5a06172b2dbacc937e9f3891de2e8951bbdc
Troels F. Rønnow <troels@wonop.com>
2023-05-30 12:46:12 +0200

    Upgrading Zilliqa and fixing issues

---
+++ build/precache-manifest.0a48b6cd7c86e73971424461912b0325.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a493870e6fba4274718d89d32dc64f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43b7e973f38865898cc",
+    "url": "/static/css/2.b98606c2.chunk.css"
+  },
+  {
+    "revision": "2d3f317c8d8abefa4ea1",
+    "url": "/static/css/main.60732dc4.chunk.css"
+  },
+  {
+    "revision": "d43b7e973f38865898cc",
+    "url": "/static/js/2.f4aad6ad.chunk.js"
+  },
+  {
+    "revision": "d918acbab0096e8674f6f089dcb62f41",
+    "url": "/static/js/2.f4aad6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3f317c8d8abefa4ea1",
+    "url": "/static/js/main.4828bd5b.chunk.js"
+  },
+  {
+    "revision": "06d9674af886c7d155bf",
+    "url": "/static/js/runtime-main.acf1573e.js"
+  },
+  {
+    "revision": "9c08883c631eaa835f20f8c97cf2f366",
+    "url": "/static/media/AvenirNextLTPro-Bold.9c08883c.woff"
+  },
+  {
+    "revision": "a7c9f661684ec1c6646190e0b1666327",
+    "url": "/static/media/AvenirNextLTPro-Bold.a7c9f661.woff2"
+  },
+  {
+    "revision": "fcf681afddd08fe66c2057b37859e567",
+    "url": "/static/media/AvenirNextLTPro-Bold.fcf681af.ttf"
+  },
+  {
+    "revision": "4f9fcb94b71f3a0d9c0f745948225fcb",
+    "url": "/static/media/AvenirNextLTPro-Demi.4f9fcb94.woff"
+  },
+  {
+    "revision": "896aaded6be2e76ff9e27c40b4f85d18",
+    "url": "/static/media/AvenirNextLTPro-Demi.896aaded.woff2"
+  },
+  {
+    "revision": "e44e6814aa380ce6e56fd3cd9e4f8127",
+    "url": "/static/media/AvenirNextLTPro-Demi.e44e6814.ttf"
+  },
+  {
+    "revision": "370e5b054b41385457578b5a1eb87ad5",
+    "url": "/static/media/AvenirNextLTPro-It.370e5b05.woff2"
+  },
+  {
+    "revision": "dd23cf1897539eb7a2dee4c3c9939cad",
+    "url": "/static/media/AvenirNextLTPro-It.dd23cf18.ttf"
+  },
+  {
+    "revision": "e165f750700e08955e6882e51e5045de",
+    "url": "/static/media/AvenirNextLTPro-It.e165f750.woff"
+  },
+  {
+    "revision": "332595fc16f573e6905695d795625188",
+    "url": "/static/media/AvenirNextLTPro-Regular.332595fc.ttf"
+  },
+  {
+    "revision": "9702fb8657b68c0201deeb690cd3b463",
+    "url": "/static/media/AvenirNextLTPro-Regular.9702fb86.woff2"
+  },
+  {
+    "revision": "d5164cfb86a2275ce1a66c5a8d25a756",
+    "url": "/static/media/AvenirNextLTPro-Regular.d5164cfb.woff"
+  },
+  {
+    "revision": "2a24ec3d394c47dc2b85e60708b49b74",
+    "url": "/static/media/bg_circle.2a24ec3d.svg"
+  },
+  {
+    "revision": "affef29476a59340fe7e6b2165bcbc78",
+    "url": "/static/media/bg_circle.affef294.svg"
+  },
+  {
+    "revision": "4ae3ba02c285dd90fbdedc5410924b41",
+    "url": "/static/media/swap_img0.4ae3ba02.png"
+  },
+  {
+    "revision": "61ffbf7a3b8796c959b03512a24b4be5",
+    "url": "/static/media/swap_img1.61ffbf7a.png"
+  },
+  {
+    "revision": "67cf2790ef08a5718e3de4304295f203",
+    "url": "/static/media/swap_img2.67cf2790.png"
+  },
+  {
+    "revision": "40c58efcffe9fcacff216a00b59c4a3d",
+    "url": "/static/media/swap_img3.40c58efc.png"
+  },
+  {
+    "revision": "a17cd78fe27259fc887a88f3b502b3da",
+    "url": "/static/media/swap_img4.a17cd78f.png"
+  },
+  {
+    "revision": "4403566181cbc4f1a1d8babb24f2b846",
+    "url": "/static/media/swap_img5.44035661.png"
+  },
+  {
+    "revision": "a0d14c67c0973c82ff1b264e906f344e",
+    "url": "/static/media/zillion.a0d14c67.svg"
+  },
+  {
+    "revision": "a66116bf94dcc0304357c7fd948c2f46",
+    "url": "/static/media/zillion.a66116bf.svg"
+  }
+]);
